81cc72866429ec478ac8b8f3a3e98182907f845a
SoominYim <soo15soo@kakao.com>
2023-09-04 18:20:51 +0900

    Client Store Update

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,38 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import SignUp from "./modules/SignUp";
+import EmitterService from "./modules/EmitterService";
+import RouteUrl from "./modules/RouteUrl";
+import Common from "./modules/Common";
+import User from "./modules/User";
+import Chat from "./modules/Chat";
+import NewBid from "./modules/NewBid";
+import CollectorService from "./modules/CollectorService";
+
+// 새로고침시 vuex 데이터 초기화 방지
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+const store = new Vuex.Store({
+    modules: {
+        SignUp: SignUp,
+        EmitterService: EmitterService,
+        RouteUrl: RouteUrl,
+        Common: Common,
+        User: User,
+        NewBid: NewBid,
+        CollectorService: CollectorService,
+        Chat: Chat,
+    },
+    plugins: [
+        createPersistedState({
+            path: ["SignUp", "EmitterService", "RouteUrl", "Common", "User", "NewBid", "CollectorService", "Chat"],
+        }),
+    ],
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
 });
+
+export default store;
